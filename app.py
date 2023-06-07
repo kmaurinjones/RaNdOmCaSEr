6ed6deba12783ba222dfec44225a5fc38d8564f9
KAI MAURIN-JONES <kmaurinj@student.ubc.ca>
2023-06-06 21:26:50 -0700

    updates

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
-# import necessary libraries
 import streamlit as st
 import random
+import SessionState
 
-def random_case(text):
-    # Convert string to list to use list methods
-    text = list(text)
-    for i in range(len(text)):
-        # Randomly decide if the character should be uppercase or lowercase
-        if random.choice([True, False]):
-            text[i] = text[i].upper()
+def randomize_casing(text):
+    result = ''
+    for char in text:
+        if random.randint(0, 1):
+            result += char.upper()
         else:
-            text[i] = text[i].lower()
-    return "".join(text)
+            result += char.lower()
+    return result
 
-st.title(random_case("RaNdOmCaSEr"))
+st.title(randomize_casing("RaNdOmCaSEr"))
 
-# Text input box
-user_input = st.text_input("Enter your text here:")
+# Informative text that doesn't change upon refresh
+st.write(randomize_casing("Enter any text below to have its casing randomized:"))
+
+# Consistent prompt text for text area
+user_input = st.text_area("Enter text:", height = 200)
+
+# Store user_input in a session state so it can be accessed even after reruns
+state = SessionState.get(user_input=user_input)
 
 if user_input:
-    # Apply random casing to the input string
-    result = random_case(user_input)
+    # Update the stored user_input whenever new input is entered
+    state.user_input = user_input
 
-    # Display the result
-    # st.text("Your text with random casing:")
-    st.write(result)
+button_text = randomize_casing("rAndOMiZe cAsiNg")
+if st.button(button_text):
+    randomized_text = randomize_casing(state.user_input)
+    st.text_area("Randomized Text:", randomized_text, height = 200)
