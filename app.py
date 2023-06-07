719309fa339d34a8f0f0fdd924b088c2bd34b1ca
KAI MAURIN-JONES <kmaurinj@student.ubc.ca>
2023-06-06 21:30:04 -0700

    updates

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+
 import streamlit as st
 import random
-import SessionState
 
 def randomize_casing(text):
     result = ''
@@ -19,14 +19,8 @@ st.write(randomize_casing("Enter any text below to have its casing randomized:")
 # Consistent prompt text for text area
 user_input = st.text_area("Enter text:", height = 200)
 
-# Store user_input in a session state so it can be accessed even after reruns
-state = SessionState.get(user_input=user_input)
-
-if user_input:
-    # Update the stored user_input whenever new input is entered
-    state.user_input = user_input
-
 button_text = randomize_casing("rAndOMiZe cAsiNg")
 if st.button(button_text):
-    randomized_text = randomize_casing(state.user_input)
-    st.text_area("Randomized Text:", randomized_text, height = 200)
+    randomized_text = randomize_casing(user_input)
+    # st.write(f"Randomized Text: {randomized_text}")
+    st.text_area(randomized_text, height = 200)
