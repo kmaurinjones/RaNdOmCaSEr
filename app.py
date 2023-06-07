a27bb15e22b219a50ec4930bc63b6b7af1be393f
KAI MAURIN-JONES <kmaurinj@student.ubc.ca>
2023-06-06 21:38:21 -0700

    updates

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# import necessary libraries
 import streamlit as st
 import random
 
@@ -16,18 +15,13 @@ def random_case(text):
 # Streamlit code
 st.title(random_case('randomcaser'))
 
-instruction_text = random_case("Enter your text here:")
-
 # Text input box
-user_input = st.text_input(instruction_text)
+user_input = st.text_input("Enter your text here:")
 
 if user_input:
     # Apply random casing to the input string
     result = random_case(user_input)
 
     # Display the result
-    # st.text(random_case("Your text with random casing:"))
+    # st.text("Your text with random casing:")
     st.write(result)
-
-    st.write("\n")
-    st.write(random_case("If you like this app, consider leaving a ⭐ on the GitHub repo. If you have any feedback or would like any additions to be made to the app, feel free to email me at kmaurinjones@gmail.com."))
